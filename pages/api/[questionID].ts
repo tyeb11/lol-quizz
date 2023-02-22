7d7c401a454292d75d5f95aed7c107cd16e44c7d
tyeb11 <tayyeb.merchant@gmail.com>
2023-02-23 01:55:08 +0530

    index(add): hero

---
+++ pages/api/[questionID].ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { questions } from "@/db.json";
+
+interface OptionObject {
+  id: Number;
+  title: String;
+}
+
+interface QuestionObject {
+  id: Number;
+  img: String;
+  title: String;
+  answer: Number;
+  options: Array<OptionObject>;
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { questionID } = req.query;
+  if (req.method == "GET") {
+    const comment: QuestionObject = questions.find(
+      ({ id }) => id == questionID
+    );
+    res.status(200).json(comment);
+  }
+}
